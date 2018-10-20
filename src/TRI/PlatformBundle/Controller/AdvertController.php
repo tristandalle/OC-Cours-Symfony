ccc85c17fbc9ee8c510f7ecb1133eeeebfb31427
tristandalle <tristandalle@gmail.com>
2018-10-20 15:43:14 +0200

    routes

--- src/TRI/PlatformBundle/Controller/AdvertController.php
+++ src/TRI/PlatformBundle/Controller/AdvertController.php
@@ -10,11 +10,25 @@ use Symfony\Component\HttpFoundation\Response;
 
 class AdvertController extends Controller
 {
+    public function viewAction($id)
+    {
+        return new Response("Affichage de l'annonce d'id :".$id);
+    }
+
+    public function viewSlugAction($slug, $year, $_format)
+    {
+        return new Response(
+            "On pourrait afficher l'annonce correspondant au slug '".$slug."', créée en ".$year." et au format ".$_format."."
+        );
+    }
+
     public function indexAction()
     {
-        $content = $this->get('templating')
-            ->render('TRIPlatformBundle:Advert:index.html.twig', array('nom' => 'Biby'));
-
-        return new Response($content);
+        $url = $this->get('router')->generate(
+            'tri_platform_view',
+            array('id' => 5)
+    );
+        return new Response("L'URL de l'annonce d'id 5 est : ".$url);
     }
+
 }
