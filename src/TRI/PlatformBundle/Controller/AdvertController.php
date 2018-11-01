6fd90523881711d539dbb314ee09158b94084251
tristandalle <tristandalle@gmail.com>
2018-11-01 15:17:03 +0100

    create views and layout

--- src/TRI/PlatformBundle/Controller/AdvertController.php
+++ src/TRI/PlatformBundle/Controller/AdvertController.php
@@ -17,23 +17,62 @@ class AdvertController extends Controller
         if($page < 1) {
             throw new NotFoundHttpException('Page"'.$page.'" inexistante.');
         }
-        return $this->render('TRIPlatformBundle:Advert:index.html.twig');
+
+        $listAdverts = array(
+            array(
+                'title'   => 'Recherche développpeur Symfony',
+                'id'      => 1,
+                'author'  => 'Alexandre',
+                'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…',
+                'date'    => new \Datetime()),
+            array(
+                'title'   => 'Mission de webmaster',
+                'id'      => 2,
+                'author'  => 'Hugo',
+                'content' => 'Nous recherchons un webmaster capable de maintenir notre site internet. Blabla…',
+                'date'    => new \Datetime()),
+            array(
+                'title'   => 'Offre de stage webdesigner',
+                'id'      => 3,
+                'author'  => 'Mathieu',
+                'content' => 'Nous proposons un poste pour webdesigner. Blabla…',
+                'date'    => new \Datetime())
+        );
+
+        return $this->render('TRIPlatformBundle:Advert:index.html.twig', array(
+            'listAdverts' => $listAdverts
+        ));
     }
 
 
     public function viewAction($id)
     {
-        return $this->render('TRIPlatformBundle:Advert:view.html.twig', array('id' => $id));
+        $advert = array(
+            'title'   => 'Recherche développpeur Symfony2',
+            'id'      => $id,
+            'author'  => 'Alexandre',
+            'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
+            'date'    => new \Datetime()
+        );
+
+        return $this->render('TRIPlatformBundle:Advert:view.html.twig', array(
+            'advert' => $advert
+        ));
     }
 
 
-    public function addAction($request)
+    public function addAction(Request $request)
     {
        if($request->isMethod('POST')){
            $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
-           return $this->redirectToRoute('tri_platform_view', array('id' => 5));
+
+           return $this->redirectToRoute('tri_platform_view', array(
+               'id' => 5
+           ));
        }
-       return $this->render('TRIPlatformBundle:Advert:add.html.twig');
+
+        return $this->render('TRIPlatformBundle:Advert:add.html.twig');
+
     }
 
     public function editAction($id, Request $request)
@@ -42,11 +81,34 @@ class AdvertController extends Controller
             $request->getSession()->getFlashBag()->add('notice', 'Annonce bien modifiée.');
             return $this->redirectToRoute('tri_platform_view', array('id' => 5));
         }
-        return $this->render('TRIPlatformBundle:Advert:edit.html.twig');
+        $advert = array(
+            'title'   => 'Recherche développpeur Symfony',
+            'id'      => $id,
+            'author'  => 'Alexandre',
+            'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…',
+            'date'    => new \Datetime()
+        );
+
+        return $this->render('TRIPlatformBundle:Advert:edit.html.twig', array(
+            'advert' => $advert
+        ));
     }
 
     public function deleteAction($id)
     {
         return $this->render('TRIPlatformBundle:Advert:delete.html.twig');
+    }
+
+    public function menuAction()
+    {
+        $listAdverts = array(
+            array('id' => 2, 'title' => 'Recherche dev Symfony'),
+            array('id' => 5, 'title' => 'Mission webmaster'),
+            array('id' => 9, 'title' => 'Offre de stage webdesigner'),
+        );
+
+        return $this->render('TRIPlatformBundle:Advert:menu.html.twig', array(
+            'listAdverts' => $listAdverts
+        ));
     }
 }
